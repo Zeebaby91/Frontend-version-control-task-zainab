de30741f80f7b0ef14c2595672e6505f4880ab61
Zainab Abdulkarim <89451161+Zeebaby91@users.noreply.github.com>
2025-11-13 11:27:02 +0100

    Update script.js
    Added arithmetic operations (+, -, *, /)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-    const calculatorDisplay = document.querySelector('h1');
+    const calculatorDisplay = document.querySelector('h2');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
@@ -82,3 +82,4 @@ function resetAll() {
 
 // Event Listener
 clearBtn.addEventListener('click', resetAll);
+
